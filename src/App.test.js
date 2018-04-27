be77a278a7ec609baa75fc7aa9e32f1adfe2b45d
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2018-04-28 01:08:32 +0200

    fix enzyme

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { shallow } from "enzyme";
+import Enzyme, { shallow } from "enzyme";
 import App from "./App";
 
-//import Adapter from "enzyme-adapter-react-15";
+import Adapter from "enzyme-adapter-react-16";
+
+Enzyme.configure({ adapter: new Adapter() });
 
 it("renders without crashing", () => {
   const div = document.createElement("div");
